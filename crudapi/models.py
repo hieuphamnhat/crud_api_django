0bc4f77a14cd595f4d317a38e07febd6939ba065
hieupn <phamnhathieu_t62@hus.edu.vn>
2021-05-11 17:28:50 +0700

    rela: one-to-one

--- crudapi/models.py
+++ crudapi/models.py
@@ -48,7 +48,35 @@ class Reporter(models.Model):
 class Reports(models.Model):
     headline = models.CharField(max_length=100)
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.headline
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s the place" % self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place, 
+        on_delete=models.CASCADE,
+        primary_key=True)
+    hot_dogs = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.place.name
+
+class Waiter(models.Model):
+    restaurant = models.ForeignKey(Restaurant,
+    on_delete = models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s the waiter at %s" % (self.name, self.restaurant)
+        
+    
 
     
